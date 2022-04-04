fd65dd498f888cdb601c4649e84f1c9779b63a9a
Your Name <felix.acevedo.tech@gmail.com>
2022-04-03 22:38:02 -0400

    employee tests passed. Added employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,14 +1,30 @@
-
-const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 //employee is like potion from rpg 
 
 test('creates an employee object', () => {
-    const employee = new Employee('Felix');
-
-    expect(employee.name).toBe('Felix');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
+    const testEmp = 'Felix';
+    const emp = new Employee(testEmp);
+    expect(emp.name).toBe(testEmp);
 });
+
+test('sets an employee id', () => {
+    const testId = 123;
+    const emp = new Employee('Felix', testId)
+    expect(emp.id).toBe(testId);
+});
+
+test('sets an employee email', () => {
+    const testEmail = 'elmalogato@hotmail.com';
+    const emp = new Employee('Felix', 123, testEmail)
+    expect(emp.email).toBe(testEmail);
+});
+
+test('sets a role for the employee', () => {
+    const testRole = 'Engineer';
+    const emp = new Employee('Felix', 123, 'elmalogato@hotmail.com', testRole);
+    expect(emp.role).toBe(testRole);
+})
+
+
 
