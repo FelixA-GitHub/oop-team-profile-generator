1d128b2539ef7f78414ffcc58159bb60c7925edc
Your Name <felix.acevedo.tech@gmail.com>
2022-04-05 21:32:20 -0400

    Manager test and js completed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,20 @@
 const Manager = require('../lib/manager');
+
+//tests if manager obj is created
+test('creates an intern object', () => {
+    const testMan = 'Felix';
+});
+
+//tests if office number is inputted
+test('checks if office number is inputted', () => {
+    const testOffNum = '888-888-8888';
+    const man = new Manager('Felix', 123, 'elmalogato@hotmail.com', 'Manager',testOffNum);
+    expect(man.offNum).toBe(testOffNum);
+});
+
+//tests if getRole() returns 'Manager'
+test('checks if getRole() overrides to Manager', () => {
+    const testRole = 'Manager';
+    const man = new Manager('Felix', 123, 'elmalogato@hotmail.com', testRole, '888-888-8888');
+    expect(man.role).toBe(testRole);
+});
