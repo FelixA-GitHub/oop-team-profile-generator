f39b655ba69190c30a02801c8cf06989739fe9dc
Your Name <felix.acevedo.tech@gmail.com>
2022-04-08 14:05:12 -0400

    testing writeFile

--- index.js
+++ index.js
@@ -174,5 +174,68 @@ const employeeQuestions = () => {
                 }
             })
         }
+
+    //writeFile and copyFile 
+    const writeFile = fileContent => {
+        //new creates new Promise object which acts like a container
+        //that allows us to run code that at some point will be pending
+        //or 'waiting for a response'
+        //useful for http requests like fetch() or setTimeout()
+        return new Promise((resolve, reject) => {
+            fs.writeFile('./dist/index.html', fileContent, err => {
+              // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+              if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+              }
+        
+              // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+              resolve({
+                ok: true,
+                message: 'File created!'
+              });
+            });
+          });
+        };
+    
+    const copyFile = () => {
+        return new Promise((resolve, reject) => {
+            fs.copyFile('./dist/index.html', fileContent, err => {
+                // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+                if (err) {
+                  reject(err);
+                  // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                  return;
+                }
+          
+                // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+                resolve({
+                  ok: true,
+                  message: 'File copied!'
+                });
+              });
+            });
+          };
+
+
+    managerQuestions().then(() => {
+        return employeeQuestions();
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+      })
+      .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+      })
+      .then(copyFileResponse => {
+        console.log(copyFileResponse);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    
+
     // )}
 
